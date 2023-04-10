dc51a6e86885e0b044edd10a5f522e912c850777
Letnys <120330424+Letnys@users.noreply.github.com>
2023-04-10 17:47:47 +0200

    removed unseless fonction

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: egeorgel <egeorgel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:46:54 by vkuzmin           #+#    #+#             */
-/*   Updated: 2023/04/10 17:04:49 by egeorgel         ###   ########.fr       */
+/*   Updated: 2023/04/10 17:46:13 by egeorgel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,11 @@ void	signal_handler(int sig)
 	if (sig == SIGINT)
 	{
 		ft_fprintf(2, "\n");
-		if (!g_prompt || !*g_prompt)
+		if (!g_prompt)
 		{
 			rl_on_new_line();
 			rl_replace_line("", 0);
 			rl_redisplay();
 		}
-	}
-}
-
-void	empty_signal_handler(int sig)
-{
-	if (sig == SIGINT)
-	{
-		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		rl_replace_line("", 0);
-		rl_on_new_line();
 	}
 }
