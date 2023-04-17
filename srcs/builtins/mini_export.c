6b68294fca2362eac9dd51f879ffa6f800bd42f6
Volodymyr Kuzmin <vkuzmin@c2r5p3.42nice.fr>
2023-04-17 18:30:26 +0200

    fixed check_input

--- srcs/builtins/mini_export.c
+++ srcs/builtins/mini_export.c
@@ -5,41 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vkuzmin <vkuzmin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 19:03:25 by vkuzmin           #+#    #+#             */
-/*   Updated: 2023/04/17 18:11:47 by vkuzmin          ###   ########.fr       */
+/*   Created: 2023/03/03 09:03:25 by vkuzmin           #+#    #+#             */
+/*   Updated: 2023/04/17 18:29:35 by vkuzmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static bool	check_input(char **str, t_data *data)
+static bool	check_input(char *str, t_data *data)
 {
 	int	i;
-
+	
 	i = 0;
-	if (!str[1] || str[2])
+	if (str[0] >= '0' && str[0] <= '9')
 	{
-		data->cmd_status = 2;
-		error(ERR_ARGS, str[0], NULL, data);
+		error(ERR_EXP, str, str, data);
 		return (false);
 	}
-	if (str[1][0] >= '0' && str[1][0] <= '9')
+	while (str[i] && str[i] != '=')
 	{
-		error(ERR_EXP, str[0], str[1], data);
-		return (false);
-	}
-	while (str[1][i] && str[1][i] != '=')
-	{
-		if ((str[1][i] >= '0' && str[1][i] <= '9')
-			|| (str[1][i] >= 'a' && str[1][i] <= 'z')
-			|| (str[1][i] >= 'A' && str[1][i] <= 'Z') || str[1][i] == '_')
+		if ((str[i] >= '0' && str[i] <= '9')
+			|| (str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z') || str[i] == '_')
 			i++;
 		else
 		{
-			error(ERR_EXP, str[0], str[1], data);
+			error(ERR_EXP, str, str, data);
 			return (false);
 		}
 	}
+	if (str[i] != '=')
+		return (false);
 	return (true);
 }
 
@@ -51,15 +47,17 @@ void	mini_export(char **str, t_data *data)
 
 	i = 1;
 	current = data->env;
-	if (!check_input(str, data))
-		return ;
 	while (current->next->next != NULL)
 		current = current->next;
 	while (str[i])
 	{
-		buf = create_node(str[i++]);
-		buf->next = current->next;
-		current->next = buf;
+		if (check_input(str[i], data))
+		{
+			buf = create_node(str[i]);
+			buf->next = current->next;
+			current->next = buf;
+		}
+		i++;
 	}
 	return ;
 }
