c3e3fa540016a5ffda022feb4b7a98d8356cfca9
Volodymyr Kuzmin <vkuzmin@c2r5p3.42nice.fr>
2023-04-17 19:25:38 +0200

    mini unset modification

--- srcs/builtins/mini_unset.c
+++ srcs/builtins/mini_unset.c
@@ -3,43 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   mini_unset.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: egeorgel <egeorgel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vkuzmin <vkuzmin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 21:00:16 by vkuzmin           #+#    #+#             */
-/*   Updated: 2023/03/30 15:58:07 by egeorgel         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:24:29 by vkuzmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static bool	check_input(char **str, t_data *data)
+static bool	check_input(char *str, t_data *data)
 {
 	int	i;
-
+	
 	i = 0;
-	if (!str[1] || str[2])
+	if (str[0] >= '0' && str[0] <= '9')
 	{
-		data->cmd_status = 2;
-		error(ERR_ARGS, str[0], NULL, data);
+		error(ERR_EXP, str, str, data);
 		return (false);
 	}
-	if (str[1][0] >= '0' && str[1][0] <= '9')
+	while (str[i])
 	{
-		error(ERR_EXP, str[0], str[1], data);
-		return (false);
-	}
-	while (str[1][i])
-	{
-		if ((str[1][i] >= '0' && str[1][i] <= '9')
-			|| (str[1][i] >= 'a' && str[1][i] <= 'z')
-			|| (str[1][i] >= 'A' && str[1][i] <= 'Z') || str[1][i] == '_')
+		if ((str[i] >= '0' && str[i] <= '9')
+			|| (str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z') || str[i] == '_')
 			i++;
 		else
 		{
-			error(ERR_EXP, str[0], str[1], data);
+			error(ERR_EXP, str, str, data);
 			return (false);
 		}
 	}
+	if (str[i] == '=')
+		return (false);
 	return (true);
 }
 
@@ -47,16 +43,25 @@ void	mini_unset(t_data *data, char **str)
 {
 	t_env	*env;
 	t_env	*prev;
+	int		i;
 
-	if (!check_input(str, data))
-		return ;
-	env = get_in_env(data, str[1]);
-	prev = get_prev_in_env(data, str[1]);
-	if (!env)
-		return ;
-	prev->next = env->next;
-	free(env->full_string);
-	free(env->pref);
-	free(env->string);
-	free(env);
+	i = 1;
+	while (str[i])
+	{
+		if (check_input(str[i], data))
+		{
+			env = get_in_env(data, str[i]);
+			prev = get_prev_in_env(data, str[i]);
+			if (!env)
+				return ;
+			prev->next = env->next;
+			free(env->full_string);
+			free(env->pref);
+			free(env->string);
+			free(env);
+			i++;
+		}
+		else
+			i++;
+	}
 }
